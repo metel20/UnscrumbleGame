fecb5c2695b66d15c86a4b99f694a007ac81e1d5
VSadeev <metel20@gmail.com>
2024-01-18 21:32:37 +0300

    ErrorPage

---
+++ app/src/androidTest/java/com/example/unscrumblegame/ErrorPage.kt
@@ -0,0 +1,24 @@
+package com.example.unscrumblegame
+
+import android.widget.LinearLayout
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import com.google.android.material.textfield.TextInputLayout
+import org.hamcrest.CoreMatchers.allOf
+
+class ErrorPage {
+    fun checkVisible() {
+        onView(
+            allOf(
+                withId(R.id.inputLayout),
+                isAssignableFrom(TextInputLayout::class.java),
+                withParent(withId(R.id.rootLayout)),
+                withParent(isAssignableFrom(LinearLayout::class.java))
+            )
+        ).check(matches(InputErrorMatcher("wrong guess")))
+    }
+
+}
