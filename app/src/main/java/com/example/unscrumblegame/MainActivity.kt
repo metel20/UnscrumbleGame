cea17eaa6510d828b3ccb936442869522aed5bae
VSadeev <metel20@gmail.com>
2024-01-18 21:59:54 +0300

    MainActivity

--- app/src/main/java/com/example/unscrumblegame/MainActivity.kt
+++ app/src/main/java/com/example/unscrumblegame/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.unscrumblegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.ViewModel
+import com.example.unscrumblegame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = GameViewModel()
+        binding.submitButton.setOnClickListener{
+            val uiState = viewModel.submit(binding.inputEditText.text.toString())
+            uiState.show(binding)
+        }
+        binding.skipButton.setOnClickListener {
+            val uiState = viewModel.skip()
+            uiState.show(binding)
+        }
+        binding.inputEditText.doAfterTextChanged {
+            val uiState = viewModel.update(binding.inputEditText.text.toString())
+            uiState.show(binding)
+        }
+        val uiState = viewModel.init()
+        uiState.show(binding)
+
+
+
+
     }
 }
